4373f0c760198ce4183c53174adb4ca68939fdc0
Chris Cheetham <chris@cheetham.com>
2026-06-09 12:39:05 -0400

    port options from old config

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,39 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.winborder = 'rounded'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.signcolumn = 'yes'
+
+vim.opt.confirm = true
+vim.opt.winborder = 'rounded'
+vim.opt.inccommand = 'split'
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+vim.opt.breakindent = true
+
+vim.opt.list = true
+vim.opt.listchars = {
+  extends  = '>',
+  nbsp     = '␣',
+  precedes = '<',
+  tab      = '» ',
+  trail    = '·',
+}
+
+vim.opt.ignorecase =true
+vim.opt.smartcase =true
+
+vim.opt.undofile = true
+
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 600
