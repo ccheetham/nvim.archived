8b713494bee20793f0a8b80a87c3dd89a4b7036e
Chris Cheetham <chris@cheetham.com>
2026-06-10 09:15:29 -0400

    add diagnostic feedback support

--- lua/ide/init.lua
+++ lua/ide/init.lua
@@ -1,3 +1,19 @@
+vim.diagnostic.config {
+  update_in_insert = false,
+  severity_sort = true,
+  float = { source = 'if_many' },
+  underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+  virtual_text = not vim.g.diag_beneath_line, -- diag at end of line
+  virtual_lines = vim.g.diag_beneath_line,    -- diag beneath line
+
+  jump = {
+    on_jump = function (_, bufnr)
+      vim.diagnostic.open_float { bufnr = bufnr, scope = 'cursor', focus = false }
+    end
+  }
+}
+
 require(submod(..., 'treesitter'))
 require(submod(..., 'lsp'))
 require(submod(..., 'format'))
--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.g.have_nerd_font = true
+vim.g.diag_beneath_line = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
